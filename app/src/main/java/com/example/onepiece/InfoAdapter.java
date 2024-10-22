9c9339ec19a02b1831e4ed33d80596e012154a49
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-10-23 00:29:17 +0200

    a

--- app/src/main/java/com/example/onepiece/InfoAdapter.java
+++ app/src/main/java/com/example/onepiece/InfoAdapter.java
@@ -1,2 +1,36 @@
-package com.example.onepiece;public class InfoAdapter {
-}
+package com.example.onepiece;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.ads.mediationtestsuite.viewmodels.InfoViewHolder;
+
+public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.InfoViewHolder>  {
+
+
+    @NonNull
+    @Override
+    public InfoAdapter.InfoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull InfoAdapter.InfoViewHolder holder, int position) {
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+
+
+}//END INFOADAPTER
